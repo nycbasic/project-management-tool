6370ad22aa5ebdc631c1892cc17e7f3b96b0fad4
nycbasic <nycbasic@gmail.com>
2019-01-27 13:21:17 -0500

    update to reset again

--- routes/api/users.js
+++ routes/api/users.js
@@ -314,7 +314,7 @@ router.post("/forgot", (req, res) => {
         subject: "Your Password Reset Confirmation",
         text: `You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n'
         'Please click on the following link, or paste this into your browser to complete the process:\n\n' +
-        'http://localhost:3000/reset/${token}\n\n'
+        'http://${process.env.PUBLIC_URL}/reset/${token}\n\n'
         'If you did not request this, please ignore this email and your password will remain unchanged.\n`
       };
 
