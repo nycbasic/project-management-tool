599cf186c7aaea96e4d995a324824cf2ce719018
Spoon <spoon@Spoons-MacBook-Air.local>
2022-06-11 17:11:27 -0400

    cleaning up users/signup helper function

--- helpers/users/index.js
+++ helpers/users/index.js
@@ -1,3 +1,11 @@
+const bcrypt = require("bcryptjs"),
+  gravatar = require("gravatar"),
+  jwt = require("jsonwebtoken"),
+  passport = require("passport"),
+  async = require("async"),
+  nodemailer = require("nodemailer"),
+  crypto = require("crypto");
+
 // Validation
 const {
   validateSignUpInput,
@@ -6,20 +14,28 @@ const {
   validatePasswordResetInput,
 } = require("../../validation/auth");
 
+// Users Model
+const Users = require("../../models/Users");
+// Password fail counter
+let passwordfailCount = 0;
+
+// Route: GET /api/users
 const home = (req, res) => {
   return res.json({
     message: "Connected to Authentication API!",
   });
 };
 
+// Route: POST /api/users/signup
 const signUp = (req, res) => {
   const { errors, isValid } = validateSignUpInput(req.body);
 
-  //   Validation Check
+  // Validation Check
   if (!isValid) {
     return res.status(400).json(errors);
   }
 
+  // MongoDB Database Query: findOne
   Users.findOne({ email: req.body.email }).then((user) => {
     if (!user) {
       const { fullName, email, password } = req.body;
@@ -39,27 +55,35 @@ const signUp = (req, res) => {
         },
       });
 
+      // Salt Password
       bcrypt.genSalt(10, (err, salt) => {
         if (err) {
           return res.send(err);
         }
+
+        // Hash & Salt Password
         bcrypt.hash(newUser.password.current, salt, (err, hash) => {
           if (err) {
             return res.send(err);
           }
+          // Not sure why I put a password fail count here, *revist*
           passwordfailCount = 0;
           newUser.password.current = hash;
           newUser.password.previous.push(hash);
 
+          // Saves the user credentials in the database
           newUser
             .save()
             .then((user) => {
               const { id, fullName, avatar } = user;
+
               const payload = {
                 id,
                 fullName,
                 avatar,
               };
+
+              // Creates a signed JWT token & sends it back to the client
               jwt.sign(
                 payload,
                 process.env.SECRET,
@@ -82,6 +106,7 @@ const signUp = (req, res) => {
         });
       });
     } else {
+      // Take a look at this
       errors.email = "User already exists!";
       return res.status(400).json(errors);
     }
